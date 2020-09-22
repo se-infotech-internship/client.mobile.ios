3918579d76402f05b25bf58f946978a0d095c6c4
toneBoogie <boogiekiev@gmail.com>
2020-09-22 17:13:10 +0300

    [IOS-1] add Splash module

---
+++ FineDriver/Modules/Splash/Presenter/AuthManager.swift
@@ -0,0 +1,17 @@
+//
+//  AuthManager.swift
+//  FineDriver
+//
+//  Created by THXDBase on 22.09.2020.
+//  Copyright © 2020 Infotekh. All rights reserved.
+//
+
+import Foundation
+
+protocol AuthManagerProtocol {
+    
+}
+
+final class AuthManager: AuthManagerProtocol {
+    
+}
---
+++ FineDriver/Modules/Splash/Presenter/UIViewController+XIB.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewController+XIB.swift
+//  FineDriver
+//
+//  Created by THXDBase on 22.09.2020.
+//  Copyright © 2020 Infotekh. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    static func initFromNib() -> Self {
+        func instanceFromNib<T: UIViewController>() -> T {
+            return T(nibName: String(describing: self), bundle: nil)
+        }
+        return instanceFromNib()
+    }
+}
